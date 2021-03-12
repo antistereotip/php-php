ba2ca7ad3fd46c422e2f6f1c582710b8b39765e0
antistereotip <milutingavrilovic@yahoo.com>
2021-03-12 07:13:40 +0100

    added php mysql dev book
    The source files of Luke Welling and Laura Thomson's "PHP and MySQL Web Development" 5th Edition book

--- funkcije.php
+++ funkcije.php
@@ -31,3 +31,42 @@ function create_table2($data, $border=1, $cellpadding=4, $cellspacing=4) {
 }
 $my_array = array('Line one', 'Line two', 'Line three');
 create_table2 ($my_array, 3, 8, 8);
+
+function test_return() {
+	echo "Ova naredba ce se izvrsiti";
+	return;
+	echo "Ova naredba se nikada nece izvrsiti";
+}
+
+
+test_return();
+
+function larger($x, $y) {
+	if(!isset($x)||!isset($y)) {
+		echo "Prosledite dva broja";
+		return;
+	}
+	if($x>$y) {
+		echo $x."<br />";
+	} else {
+		echo $y."<br />";
+	}
+	
+}
+
+larger(6,5);
+
+function var_args() {
+  echo 'Number of parameters:';
+  echo func_num_args();
+
+  echo '<br />';
+  $args = func_get_args();
+  foreach ($args as $arg) {
+    echo $arg.'<br />';
+  }
+}
+
+var_args(1,2,3);
+
+var_args('hello', 47.3);
